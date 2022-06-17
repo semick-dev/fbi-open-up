f8bda9baa04c322d9f21eb591fc8bda5539e736b
Scott Beddall <sbeddall@gmail.com>
2022-06-16 22:57:48 -0700

    massively simplify the project to a single installable package. much simpler!

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="openup",
+    name="fbiopenup",
     author="Scott Beddall",
     author_email="sbeddall@gmail.com",
     url="https://github.com/semick-dev/fbi-open-up",
-    description="The local interface to interact with the fbi agent process.",
+    description="A combined local interface and agent process that enables remote debugging of devops agents.",
     version="0.1.0",
-    packages=find_packages(where=".", exclude=["tests"]),
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["tests"]),
     install_requires=[
         "setuptools>=45.0",
-        "requests"
-        "azure-storage-blob>="
+        "requests",
+        "azure-storage-queue>=12.3.0"
     ],
     entry_points={
         "console_scripts": [
+            "openup=openup:main",
             "fbi=fbi:main",
         ]
     },
