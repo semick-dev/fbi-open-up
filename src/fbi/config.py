e941a307579c60fe4ca3941e84f3f53e3a80fe6b
Scott Beddall <sbeddall@gmail.com>
2022-07-16 22:08:42 -0700

    initial workflow (#7)

--- src/fbi/config.py
+++ src/fbi/config.py
@@ -2,4 +2,4 @@ import os
 
 QUEUE_NAME = os.getenv("FBI_QUEUE_NAME", "fbi")
 DEFAULT_CONNECTION_STRING = os.getenv("FBI_QUEUE_CS", None)
-MAX_ITERATIONS = os.getenv("FBI_MAX_ITERATIONS", 1800)
+MAX_ITERATIONS = os.getenv("FBI_MAX_ITERATIONS", 180)
